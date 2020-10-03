5914d202bff570e6bd5175237fa8a06f2c46cc5c
Ahmed Hamed <tunashark@gmail.com>
2020-10-03 16:09:42 +0100

    quick test with a header tag

---
+++ front_end/src/containers/main.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <h1>Hello from main</h1>
+        )
+    }
+
+}
+
+export default Main
